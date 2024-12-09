4d0c0c85196c4c74a7d5c3b8117b96a8a6d0376e
joao3157 <joaopaulosousa2004.js@gmail.com>
2024-12-09 13:58:44 +0000

    added orders

---
+++ src/models/orderModel.ts
@@ -0,0 +1,70 @@
+import Datastore from 'nedb';
+import path from 'path';
+
+export interface Order 
+{
+    userID: string;
+    wineIDs: string[];
+    dateOfPurchase: Date; 
+
+}
+
+export default class orderdbWorker 
+{
+    private orderDB: Nedb;
+
+    constructor() {
+        this.orderDB = new Datastore({
+            filename: path.join(__dirname, "database", "order.db"),
+            autoload: true
+        });
+    }
+
+    public insertOrder(order: Order)
+    {
+        return new Promise((resolve, reject) => 
+            {
+                this.orderDB.insert(order, (err: Error | null, newOrder: Order) => 
+                {
+                    err ? reject(err) : resolve(newOrder);
+                });
+            });
+    }
+
+    public getAllOrders(): Promise<Order[]> {
+        return new Promise((resolve, reject) => {
+            this.orderDB.find({}, (err: Error | null, orders: Order[]) => {
+                err ? reject(err) : resolve(orders);
+            });
+        });
+    }
+
+    public getOrderById(id: string): Promise<Order | null> {
+        return new Promise((resolve, reject) => {
+            this.orderDB.findOne({ _id: id }, (err: Error | null, order: Order | null) => {
+                err ? reject(err) : resolve(order);
+            });
+        });
+    }
+
+    public deleteOrder(id: string): Promise<number> {
+        return new Promise((resolve, reject) => {
+            this.orderDB.remove({ _id: id }, {}, (err: Error | null, numRemoved: number) => {
+                err ? reject(err) : resolve(numRemoved);
+            });
+        });
+    }
+
+    public filterOrders(filter: Partial<Order>): Promise<Order[]> {
+        return new Promise((resolve, reject) => {
+            if (filter.dateOfPurchase !== undefined) {
+                filter.dateOfPurchase = new Date(filter.dateOfPurchase as any);
+            } 
+
+            this.orderDB.find(filter, (err: Error | null, orders: Order[]) => {
+                err ? reject(err) : resolve(orders);
+            });
+        });
+    }
+
+}
---
+++ src/routes/orderRoutes.ts
@@ -0,0 +1,34 @@
+import express, { Router } from 'express';
+import OrderDBWorker from '../models/orderModel';
+
+const router: Router = express.Router();
+const orderWorker: OrderDBWorker = new OrderDBWorker();
+
+const handleError = (res: express.Response, error: unknown) => {
+    if (error instanceof Error) {
+        res.status(500).json({ error: error.message });
+    } else {
+        res.status(500).json({ error: "An unknown error occurred" });
+    }
+};
+
+router.get('/', async (req, res) => {
+    try 
+    {
+        if (Object.keys(req.query).length === 0) 
+        {
+            const wines = await orderWorker.getAllOrders();
+            res.status(200).json(wines);
+        } 
+        else 
+        {
+            const wines = await orderWorker.filterOrders(req.query);
+            res.status(200).json(wines);
+        }
+
+    } 
+    catch (error) 
+    {
+        handleError(res, error)
+    }
+});
